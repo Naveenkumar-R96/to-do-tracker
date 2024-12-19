d49d7f9599bb5317a220ee604fbed88ad64f8903
NAVEEN KUMAR <nk946432@gmail.com>
2024-12-19 09:42:16 +0530

    handled changes

--- src/Components/TaskColumn.jsx
+++ src/Components/TaskColumn.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import TaskCard from './TaskCard'
+import Tag from './Tag'
 const TaskColumn = ({title}) => {
   return (
-    <section className="task_column">{title}</section>
+    <section className="task_column">
+      <h2>{title}</h2>
+    <TaskCard/>
+   
+    </section>
   )
 }
 
--- src/Components/TaskForm.jsx
+++ src/Components/TaskForm.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./TaskForm.css"
 import Tag from './Tag'
 const TaskForm = () => {
+  const [task,setTask]=useState('')
+
+  const [status,setStatus]=useState('Doing')
+
+  const handleTaskChange = e =>{
+    setTask(e.target.value)
+    
+  }
+  const handleStatusChange = e =>{
+    setStatus(e.target.value)
+  }
+  console.log(task,status)
+  
   return (
     
        <header className='app_header'>
             <form action="">
-                <input type="text" className='task_input' placeholder='Enter Your task' />
+                <input type="text" 
+                className='task_input'
+                 placeholder='Enter Your task'
+                 
+                 onChange={handleTaskChange}
+                 
+                  />
                
                 <div className="task_form_bottom_line">
                    <div className='tagg'>
@@ -17,10 +36,10 @@ const TaskForm = () => {
                     <Tag title="REact"/>
                     </div>
                     <div>
-                    <select name="" id="" className="task_status">
+                    <select name="" id="" className="task_status" onChange={handleStatusChange}>
                         <option value="todo">Todo</option>
-                        <option value="todo">Doing</option>
-                        <option value="todo">Done</option>
+                        <option value="Doing">Doing</option>
+                        <option value="Done">Done</option>
                     </select>
                     </div>
                     <button type='submit' className='task_sumbit'>+Add Tasks</button>
